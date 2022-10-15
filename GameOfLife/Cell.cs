2f539850ce25299025604dbf5a7689d5ac2a4023
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2022-10-15 17:30:26 +0300

    Created class Cell and empty test for it

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Cell
+    {
+        public bool Value { get; set; }
+
+        public void SwitchValue()
+        {
+            Value = !Value;
+        }
+    }
+}
---
+++ GameOfLifeTests/CellTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Tests
+{
+    [TestClass()]
+    public class CellTests
+    {
+        [TestMethod()]
+        public void SwitchValueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
